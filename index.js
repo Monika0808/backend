af93229310bebc13ae4584fea5aef1b5a0535301
Monika0808 <149163455+Monika0808@users.noreply.github.com>
2024-03-09 05:06:17 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { MongoClient } from "mongodb";
 import { ObjectId } from "mongodb";
+import cors from "cors";
 
 const app = express();
 const PORT = 4000;
@@ -12,7 +13,8 @@ const client = new MongoClient(url);
 await client.connect();
 console.log("Database Connected Successfully");
 
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
 
 app.get("/", function (req, res) {
   res.send("Hello World!");
